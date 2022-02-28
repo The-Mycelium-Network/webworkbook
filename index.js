6f86ff196e99c4517184f9a08b795e465703b332
Schalk Neethling <sneethling@mozilla.com>
2022-02-28 18:31:51 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var http = require("http");
+
+http
+  .createServer(function (req, res) {
+    if (req.url === "/slow-response") {
+      setTimeout(() => {
+        const msg = {
+          msg: "I am as slow as a snail"
+        };
+        res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
+        res.write(JSON.stringify(msg));
+        res.end();
+      }, 3000);
+    } else {
+      const msg = {
+        msg: "Hello World"
+      };
+      res.write(JSON.stringify(msg));
+      res.end();
+    }
+  })
+  .listen(8080);
