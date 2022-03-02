ed0751a9e4778377d729b0437e1b906575853277
Schalk Neethling <sneethling@mozilla.com>
2022-03-02 23:06:43 +0200

    add route for weather data

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ http
     if (req.url === "/slow-response") {
       setTimeout(() => {
         const msg = {
-          msg: "I am as slow as a snail"
+          msg: "I am as slow as a snail",
+        };
+        res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
+        res.write(JSON.stringify(msg));
+        res.end();
+      }, 3000);
+    } else if (req.url.includes("/weather")) {
+      console.log(req.url);
+      setTimeout(() => {
+        const msg = {
+          msg: "I am as slow as a snail",
         };
         res.writeHead(200, { "Access-Control-Allow-Origin": "*" });
         res.write(JSON.stringify(msg));
@@ -13,7 +23,7 @@ http
       }, 3000);
     } else {
       const msg = {
-        msg: "Hello World"
+        msg: "Hello World",
       };
       res.write(JSON.stringify(msg));
       res.end();
