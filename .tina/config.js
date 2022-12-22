35b7fab8fcc042828e30fbe4ad6e1cb852db6f07
Schalk Neethling <schalk.neethling@mechanical-ink.co.za>
2022-12-22 22:26:20 +0200

    chore: tina config (#90)
    Tina config.

--- .tina/config.js
+++ .tina/config.js
@@ -1,46 +1,44 @@
+import { defineConfig } from "tinacms";
 
-  import { defineConfig } from "tinacms";
-  
-  // Your hosting provider likely exposes this as an environment variable
-  const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
-  
-  export default defineConfig({
-    branch,
-    clientId: null,   // Get this from tina.io
-    token: null,      // Get this from tina.io
-    build: {
-      outputFolder: "admin",
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: process.env.TINA_CLIENT_ID,
+  token: process.env.TINA_TOKEN,
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "uploads",
       publicFolder: "public",
     },
-    media: {
-      tina: {
-        mediaRoot: "uploads",
-        publicFolder: "public",
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
       },
-    },
-    schema: {
-      collections: [
-        {
-          name: "post",
-          label: "Posts",
-          path: "content/posts",
-          fields: [
-            {
-              type: "string",
-              name: "title",
-              label: "Title",
-              isTitle: true,
-              required: true,
-            },
-            {
-              type: "rich-text",
-              name: "body",
-              label: "Body",
-              isBody: true,
-            },
-          ],
-        },
-      ],
-    },
-  });
-  
+    ],
+  },
+});
